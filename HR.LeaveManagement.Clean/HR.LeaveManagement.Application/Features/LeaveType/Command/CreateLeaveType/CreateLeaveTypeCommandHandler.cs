af9ea9c402afba845de87881e30ad8c6109a87d5
Jamie Shaw <Jamie_Shaw@sagicor.com>
2023-06-25 20:24:44 -0500

    Added validation and completed CQRS pattern for Leave Types

---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Command/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Command.CreateLeaveType;
+
+public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IMapper _mapper;
+
+    public CreateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+    {
+        _leaveTypeRepository = leaveTypeRepository;
+        _mapper = mapper;
+    }
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        //Validate incoming data
+        var validator = new CreateLeaveTypeCommandValidator(_leaveTypeRepository);
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (validationResult.Errors.Any())
+            throw new BadRequestException("Invalid leave type", validationResult);
+
+        //Convert to Domain entity object
+        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+        //Add to database
+        await _leaveTypeRepository.CreateAsync(leaveTypeToCreate);
+        //Return record Id
+        return leaveTypeToCreate.Id;
+    }
+}
---
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Command/DeleteLeaveType/DeleteLeaveTypeCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Command.DeleteLeaveType;
+
+public record DeleteLeaveTypeCommand(int Id) : IRequest<Unit>;
